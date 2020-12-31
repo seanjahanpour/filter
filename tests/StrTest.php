c32dcec1ff944da4b303110544d96114631fd31b
Sean Jahanpour <sean@jahanpour.com>
2020-12-31 06:12:14 +0000

    More tests, and composer update.

--- tests/StrTest.php
+++ tests/StrTest.php
@@ -8,7 +8,7 @@ final class StrTest extends TestCase
 {
 	protected function setup(): void
 	{
-		$this->target = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()_+-=\u1000';
+		$this->target = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()_+-=\u1000";
 	}
 
 	public function test_array_exclusion_for_numbers_only(): void
@@ -18,5 +18,31 @@ final class StrTest extends TestCase
 		$result = Str::numbers_only($this->target, $exclusion);
 
 		$this->assertSame($result, '0123456789#&1000');
+	}
+
+	public function test_string_exclusion_for_numbers_only(): void
+	{
+		$exclusion = '#&';
+		
+		$result = Str::numbers_only($this->target, $exclusion);
+
+		$this->assertSame($result, '0123456789#&1000');
+	}
+
+	public function test_replacement_character_for_numbers_only(): void
+	{
+		$replacement = '_';
+		$target = '123$%^789.';
+
+		$result = Str::numbers_only($target, [],$replacement);
+
+		$this->assertSame($result, '123___789_');
+
+
+		$replacement = "','";
+		
+		$result = Str::numbers_only($target, [], $replacement);
+
+		$this->assertSame($result, "123','','','789','");
 	}
 }
